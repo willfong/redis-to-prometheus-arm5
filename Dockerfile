7845733ed07ca5058ba6de9a1f9f2a869d2a5ed1
Will Fong <willfong@pm.me>
2019-12-16 09:34:21 +0800

    Init

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM golang:latest as build
+WORKDIR /go/src/app
+COPY . .
+RUN go get -d -v ./...
+RUN go install -v ./...
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -o main .
+
+FROM scratch
+COPY --from=build /go/src/app/main /app/main
+EXPOSE 8080
+ENTRYPOINT [ "/app/main" ]
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+func main() {
+	fmt.Println("Starting service...")
+	http.HandleFunc("/", HandleRoot)
+	http.HandleFunc("/metrics", HandleMetrics)
+	http.ListenAndServe(":8080", nil)
+}
+
+// HandleRoot root
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "redis-to-prometheus")
+}
+
+// HandleMetrics redis to Prometheus
+func HandleMetrics(w http.ResponseWriter, r *http.Request) {
+	conn, err := redis.Dial("tcp", "redis:6379")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	keys, err := redis.Strings(conn.Do("KEYS", "*"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, k := range keys {
+		v, err := redis.String(conn.Do("GET", k))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "%s: %s\n", k, v)
+	}
+}
